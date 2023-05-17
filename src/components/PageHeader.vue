bcd07b4913249ea5d8468084ee2f657cec9094a4
Steve Fallet <steve.fallet@divtec.ch>
2023-05-17 10:41:38 +0200

    ajout css composant PageHeader.vue

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -25,3 +25,24 @@ defineProps({
         </a>
     </header>
 </template>
+
+<style scoped lang="sass">
+header
+    padding: 80px 0 20px
+    text-align: center
+
+header h1,
+header .description
+    font-size: 3.25rem
+    color: white
+    text-shadow: 1px 1px 2px black /* offset-x | offset-y | blur-radius | color */
+
+header  h1
+    margin: 0
+
+header .description
+    font-size: 1.5rem
+    line-height: 1.25
+    max-width: 640px
+    margin: 0 auto 1em
+</style>
